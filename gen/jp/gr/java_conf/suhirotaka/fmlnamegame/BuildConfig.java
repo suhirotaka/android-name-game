5dc97d1a442472e1e0e998a54ab7d2871a657c23
Hirotaka Suzuki <suhirotaka@gmail.com>
2016-09-17 17:44:39 +0900

    Created app

---
+++ gen/jp/gr/java_conf/suhirotaka/fmlnamegame/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package jp.gr.java_conf.suhirotaka.fmlnamegame;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
