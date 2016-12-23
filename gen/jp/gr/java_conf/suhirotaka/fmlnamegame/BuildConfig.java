664d0ab765ffcb5d01faaf27ed1995753793bd77
Hirotaka Suzuki <suhirotaka@gmail.com>
2016-12-23 18:40:30 +0900

    updated cordova

--- gen/jp/gr/java_conf/suhirotaka/fmlnamegame/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package jp.gr.java_conf.suhirotaka.fmlnamegame;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
